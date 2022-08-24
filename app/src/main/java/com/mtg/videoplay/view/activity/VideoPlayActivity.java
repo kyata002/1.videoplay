74b7a6506b8f72482838daed7ede3b3f1b901349
kyata002 <domanhhungit1@gmail.com>
2022-08-24 13:15:21 +0700

    create brith

---
+++ app/src/main/java/com/mtg/videoplay/view/activity/VideoPlayActivity.java
@@ -0,0 +1,283 @@
+package com.mtg.videoplay.view.activity;
+
+import static android.view.View.GONE;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.media.MediaMetadataRetriever;
+import android.media.MediaPlayer;
+import android.media.PlaybackParams;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.os.PowerManager;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupMenu;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.VideoView;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.mtg.videoplay.R;
+import com.mtg.videoplay.base.BaseActivity;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.LogRecord;
+import java.util.logging.SimpleFormatter;
+
+public class VideoPlayActivity extends BaseActivity {
+    ImageView bt_play,bt_pre,bt_next,bt_speed,bt_screen,bt_lock,bt_back,bt_share,bt_out;
+    TextView txt_name,txt_pstine,txt_maxtime;
+    VideoView viewvideo;
+    SeekBar pg_time;
+    LinearLayout dh_bottom,dh_top;
+    ArrayList<String> videoList;
+    Boolean ck_Dh,ck_pause;
+    ConstraintLayout videoPlay;
+    int stopPosition,position;
+    public static float speeb;
+    CountDownTimer Timer;
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_play;
+    }
+
+    @Override
+    protected void initView() {
+        bt_play = findViewById(R.id.bt_play);
+        bt_pre = findViewById(R.id.bt_prive_play);
+        bt_next = findViewById(R.id.bt_next_play);
+        bt_lock = findViewById(R.id.bt_lock_play);
+        bt_screen = findViewById(R.id.bt_phone_screen);
+        bt_speed = findViewById(R.id.bt_speed_play);
+        bt_back = findViewById(R.id.bt_back_play);
+        bt_out = findViewById(R.id.bt_zoom_out);
+        bt_share = findViewById(R.id.bt_share_play);
+        txt_maxtime = findViewById(R.id.txt_time_max);
+        txt_pstine = findViewById(R.id.txt_time_position);
+        txt_name = findViewById(R.id.txt_name_play);
+        pg_time = findViewById(R.id.pg_time_load);
+        dh_bottom = findViewById(R.id.dh_bottom);
+        dh_top = findViewById(R.id.dh_top);
+        viewvideo = findViewById(R.id.videoView);
+        videoPlay = findViewById(R.id.videoPlay);
+        position = getIntent().getIntExtra("file",1);
+        videoList=getIntent().getStringArrayListExtra("list");
+        start(position);
+//        showDH();
+        progessbar();
+//        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+
+    }
+
+    private void progessbar() {
+        pg_time.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                viewvideo.seekTo(pg_time.getProgress());
+                viewvideo.start();
+            }
+        });
+    }
+
+    private void start(int position) {
+        viewvideo.setVideoPath(videoList.get(position));
+
+        viewvideo.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                txt_maxtime.setText(fomartMaxTime(mediaPlayer.getDuration()));
+                pg_time.setMax(mediaPlayer.getDuration());
+                curentTime();
+
+                PlaybackParams myPlayBackParams = null;
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+                    myPlayBackParams = new PlaybackParams();
+                    myPlayBackParams.setSpeed(speeb); //you can set speed here
+                    mediaPlayer.setPlaybackParams(myPlayBackParams);
+                }
+            }
+        });
+
+        viewvideo.start();
+        videoPlay.isFocusable();
+        bt_play.setImageResource(R.drawable.ic_pause);
+        txt_name.setText(new File(videoList.get(position)).getName());
+//        txt_pstine.setText(curentTime(viewvideo.getCurrentPosition()));
+        showDH();
+    }
+    private void showDH(){
+
+        dh_top.setVisibility(View.VISIBLE);
+        dh_bottom.setVisibility(View.VISIBLE);
+        bt_lock.setVisibility(View.VISIBLE);
+        ck_Dh=true;
+        CountDownTimer Timer = new CountDownTimer(4000, 1000) {
+            public void onTick(long millisUntilFinished) {
+            }
+
+            public void onFinish() {
+                hideDH();
+            }
+        }.start();
+    }
+    private void hideDH(){
+        dh_top.setVisibility(GONE);
+        dh_bottom.setVisibility(GONE);
+        bt_lock.setVisibility(GONE);
+        ck_Dh=false;
+    }
+
+
+    @Override
+    protected void addEvent() {
+        videoPlay.setOnClickListener(view -> {
+            if(ck_Dh){
+                hideDH();
+            }else{
+                showDH();
+            }
+        });
+        bt_speed.setOnClickListener(view -> {
+            PopupMenu popupMenu = new PopupMenu(this,bt_speed);
+            popupMenu.getMenuInflater().inflate(R.menu.poppup_speed,popupMenu.getMenu());
+            popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem menuItem) {
+                    switch (menuItem.getItemId()){
+                        case R.id.sp05:
+                            speeb = 0.5F;
+                            break;
+                        case R.id.sp75:
+                            speeb = 0.75F;
+                            break;
+                        case R.id.sp1:
+                            speeb = 1F;
+                            break;
+                        case R.id.sp15:
+                            speeb=1.5F;
+                            break;
+                        case R.id.sp175:
+                            speeb=1.75F;
+                            break;
+                        case R.id.sp2:
+                            speeb=2F;
+                            break;
+                    }
+                    return false;
+                }
+            });
+            popupMenu.show();
+        });
+
+        dhAdmin();
+        dhTop();
+        changeScreen();
+
+    }
+
+    private void dhTop() {
+        bt_back.setOnClickListener(view -> {
+            onBackPressed();
+        });
+    }
+
+    private void dhAdmin() {
+        bt_play.setOnClickListener(view -> {
+            if(ck_pause){
+                onResume();
+            }else{
+                onPause();
+            }
+        });
+        bt_pre.setOnClickListener(view -> {
+            if(position<videoList.size()||position>0){
+                position--;
+                viewvideo.stopPlayback();
+                start(position);
+            }
+
+        });
+        bt_next.setOnClickListener(view -> {
+            if(position<videoList.size()||position==0){
+                position++;
+                viewvideo.stopPlayback();
+                start(position);
+            }
+        });
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        bt_play.setImageResource(R.drawable.ic_play);
+        stopPosition = viewvideo.getCurrentPosition(); //stopPosition is an int
+        viewvideo.pause();
+        ck_pause=true;
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        bt_play.setImageResource(R.drawable.ic_pause);
+        ck_pause=false;
+        viewvideo.seekTo(stopPosition);
+        viewvideo.start(); //Or use resume() if it doesn't work. I'm not sure
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    private String fomartMaxTime(int position){
+        SimpleDateFormat timeMax = new SimpleDateFormat("mm:ss");
+        return timeMax.format(position);
+    }
+
+    private void changeScreen(){
+        bt_screen.setOnClickListener(view -> {
+            int orientation = getResources().getConfiguration().orientation;
+            if(orientation== Configuration.ORIENTATION_LANDSCAPE){
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            }else{
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            }
+        });
+    }
+    private void curentTime(){
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                SimpleDateFormat timeMax = new SimpleDateFormat("mm:ss");
+                txt_pstine.setText(timeMax.format(viewvideo.getCurrentPosition()));
+                pg_time.setProgress(viewvideo.getCurrentPosition());
+                handler.postDelayed(this,50);
+            }
+        },100);
+    }
+
+
+}
